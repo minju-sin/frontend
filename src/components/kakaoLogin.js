50262ba27989bbb9b06e8b7c518dc209fbfa749d
minju-sin <sinminju04@gmail.com>
2024-01-11 16:08:52 +0900

    기본 frontend 설정

---
+++ src/components/kakaoLogin.js
@@ -0,0 +1,15 @@
+// src/components/kakaoLogin.js
+
+import React from "react";
+
+
+function KakaoLogin() {
+
+    return (
+        <div>
+            <p>kakao</p>
+        </div>
+    );
+}
+
+export default KakaoLogin;
